0afd8225add443d42e59626e776c8d6fbb289db3
szaim <zaimsofia81@gmail.com>
2017-01-09 13:36:51 -0500

    project finished

--- index.js
+++ index.js
@@ -13,18 +13,25 @@ function getQuote(){
 	var link = "http://api.forismatic.com/api/1.0/?method=getQuote&key=457653&format=jsonp&lang=en&jsonp=?";
   $.getJSON(link, function (data) {
 		console.log(data);
-		generateQuote(data);
-
-    
+		generateQuote(data);   
     });
 };
 
 var generateQuote = function(item) {
 	var $quote = $('.quote');
-	var $author = $('.author')
+	var $author = $('.author');
+	var colors = ["rgb(249, 150, 129)", "rgb(251, 144, 222)", "rgb(254, 162, 183)", "rgb(247, 255, 183)",
+					"rgb(190, 247, 255)", "rgb(190, 200, 255)", "rgb(254, 91, 113)", "rgb(253, 86, 190)" ];
 
+	var randomColors = colors[Math.floor(Math.random() * colors.length)];
+	if(item.quoteAuthor === "") {
+		item.quoteAuthor = "Unknown";
+	}
 	$quote.html(item.quoteText);
 	$author.html(item.quoteAuthor);
+	$('body').css('background', randomColors);
+	$('.btn').css('background', randomColors);
+	$quote.css('color', randomColors);
 }
 
 $('.btn').click(function(e) {
